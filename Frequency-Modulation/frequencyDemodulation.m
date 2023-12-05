16157afaf7014d14e5aa9984797dffb98fa35a8c
abdelrahman1503 <abdelrahman.1503h@gmail.com>
2023-12-05 21:33:50 +0200

    Documentation and Comments

--- Frequency-Modulation/frequencyDemodulation.m
+++ Frequency-Modulation/frequencyDemodulation.m
@@ -1,11 +1,24 @@
 function frequencyDemodulation(modSignal, f_Sampling, timeVector)
+    % frequencyDemodulation performs frequency demodulation on a modulated signal
+    % Inputs:
+    %   modSignal: Modulated signal to be demodulated
+    %   f_Sampling: Sampling frequency of the signal
+    %   timeVector: Time vector corresponding to the signal
+
+    % Compute the derivative of the modulated signal to demodulate
     demodSignal = diff(modSignal);
-    demodSignal = [0; demodSignal];
-    envelope = abs(hilbert(demodSignal)) - mean(abs(hilbert(demodSignal)));
+    demodSignal = [0; demodSignal]; % Compensate for length difference due to diff function
+    
+    % Obtain the envelope of the demodulated signal using the Hilbert transform
+    envelope = abs(hilbert(demodSignal)) - mean(abs(hilbert(demodSignal))); % Extract envelope
+    
+    % Plot the demodulated signal envelope over time
     figure;
     plot(timeVector, envelope);
     title("Demodulated Signal");
     
+    % Resample the envelope and play the audio
     envelope = resample(envelope, f_Sampling / 5, f_Sampling);
+    % Scale and play the envelope (500 is an arbitrary scaling factor for audio perception)
     sound(500 .* abs(envelope), f_Sampling/5);
 end
--- Frequency-Modulation/frequencyModulation.m
+++ Frequency-Modulation/frequencyModulation.m
@@ -1,17 +1,28 @@
 function [modSignal, timeVector] = frequencyModulation(kf, carrierFreq, carrierAmp, signal, f_Sampling)
+    % frequencyModulation performs frequency modulation on an input signal
+    % Inputs:
+    %   kf: Frequency deviation constant (modulation index)
+    %   carrierFreq: Carrier frequency of the modulation
+    %   carrierAmp: Amplitude of the carrier signal
+    %   signal: Input signal to be modulated
+    %   f_Sampling: Sampling frequency of the signal
+
+    % Generate the time vector corresponding to the signal
     startTime = 0;
     endTime = startTime + length(signal) / f_Sampling;
     timeVector = linspace(startTime, endTime, length(signal));
     timeVector = timeVector';
 
-    %FM modulated signal
-    modSignal = carrierAmp * cos(2 * pi * carrierFreq * timeVector + 2 * pi * kf * cumsum(signal)./ f_Sampling);
+    % Perform Frequency Modulation (FM) on the input signal to generate the modulated signal
+    modSignal = carrierAmp * cos(2 * pi * carrierFreq * timeVector + 2 * pi * kf * cumsum(signal) / f_Sampling);
 
-    % Fourier transform
+    % Compute the Fourier transform of the input signal
     len = length(signal);
     S_Freq = fftshift(fft(signal));
-    freq = f_Sampling / 2 * linspace(-1,1,len);
-    figure; 
+    freq = f_Sampling / 2 * linspace(-1, 1, len);
+
+    % Plot the spectrum of the frequency-modulated signal
+    figure;
     plot(freq, S_Freq);
-    title('Spectrum of Frequecny Modulated Signal');
+    title('Spectrum of Frequency Modulated Signal');
 end
