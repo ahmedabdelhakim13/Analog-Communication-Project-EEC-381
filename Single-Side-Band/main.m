c316b04626600ec9fdfd9daa5f6868c44210205c
ahmed abdelhakem <104411735+ahmedabdelhakim13@users.noreply.github.com>
2023-12-13 13:41:17 +0200

    step3

--- Single-Side-Band/main.m
+++ Single-Side-Band/main.m
@@ -9,18 +9,26 @@ T=1/Fs;                              %defining the periodic time for the signal
 %time domain
 t=linspace(0,length(y)/Fs,length(y));
 figure;
-subplot(1,2,1);
-plot(y,t);
-title('time domain');
-xlabel('time');
-ylabel('Amplitude');
+%subplot(1,2,1);
+%plot(t,y);
+%title('time domain');
+%xlabel('time');
+%ylabel('Amplitude');
 %frequency domain
-nfft = 2048
+nfft = length(y);
 f=linspace(0,Fs,nfft);
 Y=abs(fftshift(fft(y,nfft)));
-subplot(1,2,2);
-plot(Y,f);
+%%subplot(1,2,2);
+plot(f,Y);
 title('frequency domain');
+xlabel('frequency');
+ylabel('Amplitude');
+%%dseign ideal low pass filter
+d=designfilt('lowpassfir','FilterOrder',5,'cutoffFrequency',4000,'samplerate',Fs);
+z=filter(d,Y);
+figure;
+plot(Y);
+title('frequency filtered domain');
 xlabel('frequency');
 ylabel('Amplitude');
 %plot(abs(fft(y)),Fs); %plot  %subplot %xlabel %ylabel %title %sound(y,Fs);
