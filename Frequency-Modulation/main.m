8648259153679be66e8c0871bc8fb143131f4b0f
abdelrahman1503 <abdelrahman.1503h@gmail.com>
2023-12-23 15:39:11 +0200

    Reorganizing

--- Frequency-Modulation/main.m
+++ Frequency-Modulation/main.m
@@ -17,11 +17,11 @@ title('Filtered Signal Time Domain');
 
 sound(abs(filteredSignal), f_S);
 %% Modulating the Signal
-kf = 0.2/(2*pi*max(abs(cumsum(filteredSignal)))./ f_S);
 carrierFreq = 100000;
 carrierAmp = 1;
 filteredSignal = resample(filteredSignal, 5 * carrierFreq, f_S);
 f_S = 5 * carrierFreq;
+kf = 0.2/(2*pi*max(abs(cumsum(filteredSignal)))./ f_S);
 [modSignal, timeVector] = frequencyModulation(kf, carrierFreq, carrierAmp, filteredSignal, f_S);
 %% Demodulating the Signal
 frequencyDemodulation(modSignal, f_S, timeVector);
